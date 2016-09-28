00b73dc7fdf37c33b72e76a09546265551958c43
dbellido <becadiego@gmail.com>
2016-09-28 17:17:51 +0200

    a

--- web_pages/ALS_don_wall/libs/raycast.js
+++ web_pages/ALS_don_wall/libs/raycast.js
@@ -58,6 +58,22 @@ function onDocumentMouseDown( event )
 
 	}
 
+	var intersects_3 = raycaster.intersectObjects( [cube4] );
+
+	// if there is one (or more) intersections
+	if ( intersects_3.length > 0 )
+	{
+		//console.log("Hit right @ " + toString( intersects[0].point ) );
+		window.open("‪http://dbellido.github.io/Donaustauf_castle/Donaustauf/index.html")
+		// change the color of the closest face.
+		//intersects[0].object.callback();
+		intersects_3[ 0 ].face.color.setRGB( 0.8 * Math.random() + 0.2, 0, 0 );
+		intersects_3[ 0 ].object.geometry.colorsNeedUpdate = true;
+		winW = ""+(screen.width / 2) -200 ;
+		winH = ""+(screen.height / 2) -200 ;
+
+	}
+
 	// else{
 	// 	viewer.scenePointCloud.remove(plane);
 	// }
@@ -100,6 +116,26 @@ function onMouseMove( event ) {
 	}
 
 	//for the loggia cube	// create an array containing all objects in the scene with which the ray intersects
+	var intersects_4 = raycaster.intersectObjects( [cube4] );
+
+	// if there is one (or more) intersections
+	if ( intersects_4.length > 0 )
+	{
+
+		// change the color of the closest face.
+		intersects_4[ 0 ].object.material.color.setRGB(1,0.3,1);
+		intersects_4[ 0 ].object.material.opacity=[0.3];
+		intersects_4[ 0 ].object.geometry.colorsNeedUpdate = true;
+		plane_hs2.visible=true
+	}
+	else {
+		cube4.material.color.setRGB( 1,0,1);
+		cube4.geometry.colorsNeedUpdate = true;
+		cube4.material.opacity=[0];
+		plane_hs2.visible=false
+	}
+
+
 	var intersects_3 = raycaster.intersectObjects( [cube3] );
 
 	// if there is one (or more) intersections
@@ -107,9 +143,9 @@ function onMouseMove( event ) {
 	{
 
 		// change the color of the closest face.
-		intersects_4[ 0 ].object.material.color.setRGB(1,0.3,1);
-		intersects_4[ 0 ].object.material.opacity=[0.3];
-		intersects_4[ 0 ].object.geometry.colorsNeedUpdate = true;
+		intersects_3[ 0 ].object.material.color.setRGB(1,0.3,1);
+		intersects_3[ 0 ].object.material.opacity=[0.3];
+		intersects_3[ 0 ].object.geometry.colorsNeedUpdate = true;
 		plane_hs2.visible=true
 	}
 	else {
