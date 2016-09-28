ea11faa604a2ee7ff55917b96483b99400da85a8
dbellido <becadiego@gmail.com>
2016-09-28 16:46:43 +0200

    a

--- web_pages/ALS_don_wall/libs/raycast.js
+++ web_pages/ALS_don_wall/libs/raycast.js
@@ -30,7 +30,7 @@ function onDocumentMouseDown( event )
 	if ( intersects.length > 0 )
 	{
 		//console.log("Hit right @ " + toString( intersects[0].point ) )
-		window.open("‪../../donaustauf_village.html#Walhalla")
+		window.open("‪../../donaustauf_village.html#st_michael")
 		// change the color of the closest face.
 		//intersects[0].object.callback();
 		intersects[ 0 ].face.color.setRGB( 0.8 * Math.random() + 0.2, 0, 0 );
@@ -107,15 +107,15 @@ function onMouseMove( event ) {
 	{
 
 		// change the color of the closest face.
-		intersects_3[ 0 ].object.material.color.setRGB(1,0.3,1);
-		intersects_3[ 0 ].object.material.opacity=[0.3];
-		intersects_3[ 0 ].object.geometry.colorsNeedUpdate = true;
+		intersects_4[ 0 ].object.material.color.setRGB(1,0.3,1);
+		intersects_4[ 0 ].object.material.opacity=[0.3];
+		intersects_4[ 0 ].object.geometry.colorsNeedUpdate = true;
 		plane_hs2.visible=true
 	}
 	else {
-		cube3.material.color.setRGB( 1,0,1);
-		cube3.geometry.colorsNeedUpdate = true;
-		cube3.material.opacity=[0];
+		cube4.material.color.setRGB( 1,0,1);
+		cube4.geometry.colorsNeedUpdate = true;
+		cube4.material.opacity=[0];
 		plane_hs2.visible=false
 	}
 	plane_hs.lookAt(viewer.camera.position);
